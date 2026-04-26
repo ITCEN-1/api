b13c27f2e2ffb5ce4e6d7f94821cb33258e4e934
JoonHyuk0331 <joonlife0901@gmail.com>
2026-04-26 10:47:41 +0900

    chore: 초기 의존성 관련 세팅

---
+++ src/main/java/com/itset/itcenteamproject/ItcenTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.itset.itcenteamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItcenTeamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItcenTeamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itset/itcenteamproject/ItcenTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itset.itcenteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItcenTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
