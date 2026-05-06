07d28ad1d4c149dfc018f76cac96608062acbb0f
JoonHyuk0331 <joonlife0901@gmail.com>
2026-05-06 18:12:57 +0900

    feat: 시큐리티 필터에서 스웨거 관련 엔드포인트 제외

---
+++ src/main/java/com/itset/itcenteamproject/global/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.itset.itcenteamproject.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Locale;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // 테스트 시 편리함을 위해
+                .authorizeHttpRequests(auth -> auth
+                        // 1.Swagger 관련 리소스는 모두 허용
+                        .requestMatchers(
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/webjars/**",
+                                "/api/hello",
+                                "/api/hello/*"
+                        ).permitAll()
+                        // 2.그 외 API는 인증 필요
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
