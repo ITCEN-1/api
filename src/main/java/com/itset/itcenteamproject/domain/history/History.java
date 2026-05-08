3e7feee4a46bd41b0f80b0326b758b6bae2ac73d
JoonHyuk0331 <joonlife0901@gmail.com>
2026-05-08 19:03:52 +0900

    feat: History 엔티티

---
+++ src/main/java/com/itset/itcenteamproject/domain/history/History.java
@@ -0,0 +1,47 @@
+package com.itset.itcenteamproject.domain.history;
+
+import com.itset.itcenteamproject.domain.survey.Survey;
+import com.itset.itcenteamproject.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "histories")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class History {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "survey_id", nullable = false, unique = true)
+    private Survey survey;
+
+    @OneToMany(mappedBy = "history", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<HistoryItem> historyItems = new ArrayList<>();
+
+    @Builder
+    public History(User user, Survey survey) {
+        this.user = user;
+        this.survey = survey;
+    }
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+}
