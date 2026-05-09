4e497534d6ec35c5f5fb5b59fd58ab55fd55637f
imdhlim <dlaejrgus908@gmail.com>
2026-05-09 22:59:06 +0900

    fix: 레이어드 아키텍처에 맞도록 로직 분리 및 수정

--- src/main/java/com/itset/itcenteamproject/domain/user/UserApiController.java
+++ src/main/java/com/itset/itcenteamproject/domain/user/UserApiController.java
@@ -1,10 +1,10 @@
 package com.itset.itcenteamproject.domain.user;
 
-import com.itset.itcenteamproject.domain.survey.SurveyRepository;
+import com.itset.itcenteamproject.domain.survey.SurveyService;
 import com.itset.itcenteamproject.domain.user.dto.*;
-import com.itset.itcenteamproject.exception.CustomException;
-import com.itset.itcenteamproject.exception.ErrorCode;
-import com.itset.itcenteamproject.domain.user.UserService;
+import com.itset.itcenteamproject.domain.user.service.SessionUserService;
+import com.itset.itcenteamproject.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +14,12 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class UserApiController {
-
+    //회원가입, 로그인 관련
     private final UserService userService;
-    private final SurveyRepository surveyRepository;
+    //설문 관련
+    private final SurveyService surveyService;
+    //세션 관련
+    private final SessionUserService sessionUserService;
 
     //회원가입
     @PostMapping("/signup")
@@ -35,19 +38,18 @@ public class UserApiController {
             HttpSession session
     ) {
         User user = userService.login(dto);
-        session.setAttribute("loginUser", user.getId());
-        session.setMaxInactiveInterval(120);
+        sessionUserService.login(session, user.getId());
 
         //설문 여부 확인 후 페이지 이동(프론트)
-        boolean surveyCompleted = surveyRepository.existsByUserId(user.getId());
+        boolean surveyCompleted = surveyService.hasSurvey(user.getId());
         String redirectPath = surveyCompleted ? "/dashboard" : "/surveys";
 
-        return new LoginResponseDTO(
-                user.getLoginId(),
-                user.getNickname(),
-                surveyCompleted,
-                redirectPath
-        );
+        return LoginResponseDTO.builder()
+                .loginId(user.getLoginId())
+                .nickname(user.getNickname())
+                .surveyCompleted(surveyCompleted)
+                .redirectPath(redirectPath)
+                .build();
     }
 
     //로그인 아이디 중복 확인
@@ -55,10 +57,7 @@ public class UserApiController {
     public DupCheckResponseDTO checkLoginId(
             @RequestParam String loginId
     ) {
-        boolean duplicated = userService.checkLoginIdDuplicate(loginId);
-        if (duplicated) {
-            throw new CustomException(ErrorCode.DUPLICATE_LOGIN_ID);
-        }
+        userService.checkLoginIdDuplicate(loginId);
 
         return new DupCheckResponseDTO(true);
     }
@@ -67,8 +66,7 @@ public class UserApiController {
     @PostMapping("/auth/logout")
     public ResponseEntity<Void> logout(HttpSession session) {
         //인증 체크
-        getLoginUser(session);
-        session.invalidate();
+        sessionUserService.logout(session);
 
         return ResponseEntity.noContent().build();
     }
@@ -76,7 +74,7 @@ public class UserApiController {
     //로그인 된 사용자 정보 조회(세션 유지 테스트용)
     @GetMapping("/me")
     public UserResponseDTO me(HttpSession session) {
-        Long userId = getLoginUser(session);
+        Long userId = sessionUserService.getLoginUserId(session);
         User user = userService.findById(userId);
 
         return new UserResponseDTO(user);
@@ -84,25 +82,14 @@ public class UserApiController {
 
     //의도하지 않은 경로로 대시보드 진입 방지
     //프론트에서 대시보드 페이지 진입 전 호출
+    @Operation(description = "의도하지 않은 경로로 대시보드 진입 방지," +
+            " 프론트에서 대시보드 페이지 진입 전 호출", summary = "대시보드 진입 전 체크" )
     @GetMapping("/auth/survey-check")
     public SurveyCheckResponseDTO checkSurvey(HttpSession session) {
-        Long userId = getLoginUser(session);
-
-        boolean surveyCompleted = surveyRepository.existsByUserId(userId);
+        Long userId = sessionUserService.getLoginUserId(session);
+        boolean surveyCompleted = surveyService.hasSurvey(userId);
         String redirectPath = surveyCompleted ? "/dashboard" : "/surveys";
 
         return new SurveyCheckResponseDTO(surveyCompleted, redirectPath);
-    }
-
-
-    //인증 체크(세션에 loginUser가 있는지)
-    private Long getLoginUser(HttpSession session) {
-        //로그인 상태 -> userid 반환
-        Long userId = (Long) session.getAttribute("loginUser");
-        //세션 없음 -> 401예외
-        if (userId == null) {
-            throw new CustomException(ErrorCode.SESSION_EXPIRED);
-        }
-        return userId;
     }
 }
