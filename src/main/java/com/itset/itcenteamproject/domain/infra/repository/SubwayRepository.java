b593c5698a8eff267cc4ec83e2a43c3874757949
imdhlim <dlaejrgus908@gmail.com>
2026-05-11 23:20:46 +0900

    fix: infra Repository에 목록 조회 메서드 추가

--- src/main/java/com/itset/itcenteamproject/domain/infra/repository/SubwayRepository.java
+++ src/main/java/com/itset/itcenteamproject/domain/infra/repository/SubwayRepository.java
@@ -3,8 +3,12 @@ package com.itset.itcenteamproject.domain.infra.repository;
 import com.itset.itcenteamproject.domain.infra.entity.Subway;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubwayRepository extends JpaRepository<Subway, Long> {
 
     // 특정 동의 지하철 개수
     long countByDongCode(Integer dongCode);
+    // 목록 조회
+    List<Subway> findByDongCode(Integer dongCode);
 }
