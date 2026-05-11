fa732cdbd2eb25ae48f30b32edbc99153983497a
imdhlim <dlaejrgus908@gmail.com>
2026-05-11 23:21:37 +0900

    fix: 중복 파일 삭제

--- src/main/java/com/itset/itcenteamproject/domain/infra/DongLocationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.itset.itcenteamproject.domain.infra;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DongLocationRepository extends JpaRepository<DongLocation,Integer> {
-}
