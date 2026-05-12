0a8723187669d79503787ed2b89357dce52ec22f
imdhlim <dlaejrgus908@gmail.com>
2026-05-12 13:56:04 +0900

    fix: 중복 코드 삭제

--- src/main/java/com/itset/itcenteamproject/domain/survey/SurveyRepository.java
+++ src/main/java/com/itset/itcenteamproject/domain/survey/SurveyRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-import java.util.Optional;
-
 
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
     boolean existsByUserId(Long userId);
