721260b5b700a181dad17c138c9fcc81b5aa3e6d
JoonHyuk0331 <joonlife0901@gmail.com>
2026-05-25 15:40:31 +0900

    feat: 다중 조회를 위한 in 쿼리 추가

--- src/main/java/com/itset/itcenteamproject/domain/infra/repository/SubwayRepository.java
+++ src/main/java/com/itset/itcenteamproject/domain/infra/repository/SubwayRepository.java
@@ -3,6 +3,9 @@ package com.itset.itcenteamproject.domain.infra.repository;
 import com.itset.itcenteamproject.domain.infra.entity.Subway;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface SubwayRepository extends JpaRepository<Subway, Long> {
@@ -11,4 +14,9 @@ public interface SubwayRepository extends JpaRepository<Subway, Long> {
     long countByDongCode(Integer dongCode);
     // 목록 조회
     List<Subway> findByDongCode(Integer dongCode);
+
+    // 여러 동의 개수를 한 번에 집계 (N+1 방지)
+    @Query("select new com.itset.itcenteamproject.domain.infra.repository.DongCountDTO(s.dongCode, count(s)) " +
+            "from Subway s where s.dongCode in :codes group by s.dongCode")
+    List<DongCountDTO> countByDongCodeIn(@Param("codes") List<Integer> dongCodes);
 }
