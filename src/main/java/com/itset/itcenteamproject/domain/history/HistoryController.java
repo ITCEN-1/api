760fc402aa0f08f54c79fc3ed10601a06d4ffa4d
imdhlim <dlaejrgus908@gmail.com>
2026-05-25 16:36:01 +0900

    fix: 미사용 api 삭제

--- src/main/java/com/itset/itcenteamproject/domain/history/HistoryController.java
+++ src/main/java/com/itset/itcenteamproject/domain/history/HistoryController.java
@@ -20,18 +20,11 @@ public class HistoryController {
         this.historyService = historyService;
     }
 
-    // Pageable 어노테이션에서는 기본 PageSize: 10, pageNum: 0입니다.
-    @Operation(summary = "세션 유저의 히스토리 목록 조회")
-    @GetMapping("/histories")
-    public List<HistoryDTO> getSurveyAndHistories(@Parameter(hidden = true) @SessionAttribute("loginUser") String userId,
-                                                    @PageableDefault Pageable pageable) {
-        return historyService.getHistories(userId, pageable);
-    }
-
     @Operation(summary = "히스토리 목록 내 특정 히스토리 조회")
     @GetMapping("/history/{surveyId}")
     public HistoryDTO getSurveyAndHistory(@PathVariable Long surveyId) {
         return historyService.getHistory(surveyId);
+    }
       
     @Operation(summary = "히스토리 조회",description = "세션 유저의 히스토리 조회")
     @GetMapping("/history")
