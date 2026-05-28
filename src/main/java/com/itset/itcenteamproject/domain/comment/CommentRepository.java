9b031fa6a9afd1323a8432f6ec5b66c54dd07284
imdhlim <dlaejrgus908@gmail.com>
2026-05-28 11:24:13 +0900

    fix: 댓글 최신순으로 변경

--- src/main/java/com/itset/itcenteamproject/domain/comment/CommentRepository.java
+++ src/main/java/com/itset/itcenteamproject/domain/comment/CommentRepository.java
@@ -13,7 +13,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
      * 특정 게시글에 달린 댓글 목록을 조회한다.
      *
      * Board, User를 조인해서 댓글 목록 DTO로 바로 조회한다.
-     * 오래된 댓글이 위에 나오도록 작성일 오름차순으로 정렬한다.
+     * 댓글은 최신순(오름차순)
      */
     @Query("""
         select new com.itset.itcenteamproject.domain.comment.dto.CommentListItem(
@@ -27,7 +27,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
         from Comment c
         join c.user u
         where c.board.id = :boardId
-        order by c.createdAt asc
+        order by c.createdAt desc 
     """)
     List<CommentListItem> findCommentsByBoardId(@Param("boardId") Long boardId);
 
